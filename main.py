e99e98fbf7dc478088e933287e475f6e21973a9f
koosha lahouti <klahouti81@gmail.com>
2022-10-03 21:20:17 +0330

    Final Commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from googleapiclient.http import MediaFileUpload
+from Google import create_service
+import sched
+import time
+
+s = sched.scheduler(time.time, time.sleep)
+
+
+def repeat(sc):
+    CLIENT_SECRET_FILE = 'client-secret.json'
+    API_NAME = 'drive'
+    API_VERSION = 'v3'
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+
+    service = create_service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+    file_metadata = {
+        'name': 'google.jpg',
+        'parents': ['1PqRdx6K1Th3aCboZLH_5uAyI-jZGT9ag']
+    }
+
+    media_content = MediaFileUpload('25580.jpg', mimetype='image/jpg')
+
+    file = service.files().create(
+        body=file_metadata,
+        media_body=media_content
+    ).execute()
+    sc.enter(60, 1, repeat, (sc,))
+
+
+s.enter(60, 1, repeat, (s,))
+s.run()
